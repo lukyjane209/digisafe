adafb438d96ecbedee19a10d6c179856e0e62cfe
Lakshay Juneja <lukyjane209>
2018-03-21 18:56:30 +0530

    First commit

---
+++ WEB-INF/classes/dbbean/path1.bat
@@ -0,0 +1,3 @@
+set path=C:\Oracle\Middleware\jdk160_29\bin;
+
+set classpath=C:\Oracle\Middleware\wlserver_12.1\server\lib\weblogic.jar;C:\oraclexe\app\oracle\product\10.2.0\server\jdbc\lib\ojdbc14.jar;.
